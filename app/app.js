478003eed1b517b39db2e428db31716ed63a704c
David Wang <wyr@protonmail.com>
2019-01-14 11:22:33 +0800

    Support showing whole day date

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-const timeRange = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+const timeRange = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 
 const App = function() {
   const [time, setTime] = React.useState(0)
@@ -48,7 +48,7 @@ const App = function() {
       </div>
       <div id="toolbar">
         <input type="checkbox" checked={autoPlay} onChange={handleAutoPlayChange} /> Auto Play
-        <input type="range" min="0" max="23" value={time} onChange={handleSliderChange} className="slider" />
+        <input type="range" min="0" max="24" value={time} onChange={handleSliderChange} className="slider" />
         <span>{time}:00 - {time}:59</span>
           &nbsp;&nbsp;类型：
           <select onChange={handleTypeChange} value={type}>
